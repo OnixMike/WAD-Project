f2f8106020419368028a0b7cd50c6fa9a73e2817
Carol19 <mihailescuioancarol@yahoo.ro>
2017-05-11 19:08:43 +0300

    Add files via upload

---
+++ CreateLogin/src/java/MyServlet.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.sql.Statement;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ *
+ * @author Jon
+ */
+public class MyServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     * @throws java.sql.SQLException
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+                    ResultSet result = null; 
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            /* TODO output your page here. You may use following sample code. */
+             Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/wad","asa","asa" ) ;
+             ResultSet rs = null;
+            PreparedStatement instr =  (PreparedStatement) con.createStatement();
+            PreparedStatement nou =  (PreparedStatement) con.createStatement();
+            String myUname = request.getParameter("uname");  
+            String pass = request.getParameter("pass");
+            String lname = request.getParameter("lname"); 
+            String fname = request.getParameter("fname"); 
+            String email = request.getParameter("email");  
+            String phone = request.getParameter("phone"); 
+                         out.print(myUname);
+            result = nou.executeQuery("select fname from register where uname = myUname;");
+            if(result.next()){
+               out.println("Username already exists"); //data exists
+                    response.sendRedirect("/index.html"); 
+            }
+             else
+             {
+                 out.println("bravo");
+                 String  st="insert into data  VALUES ('"+fname+"', '"+lname+"','"+email+"','"+phone+"','"+myUname+"',"
+                    + "'"+pass+"')";
+                int i = instr.executeUpdate(st);
+            }
+             instr.close();
+             con.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(MyServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(MyServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
