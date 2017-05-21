8c126fed8f2c99955fa5df15b11360165a707229
dragoshtefan <stefan.dragos.popescu@gmail.com>
2017-05-21 18:58:19 +0300

    no message

---
+++ src/java/model/Product.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Mihai
+ */
+
+public class Product {
+
+   
+    private int id;
+    private String name;
+    private String type;
+    private int price;
+    private int qty;
+
+    public Product(int id, String name, String type, int price, int qty) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+}
